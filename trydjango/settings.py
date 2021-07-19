443f54aa63e91a9ef9fa48ff0327d68991336abb
mwaraic <59263215+mwaraic@users.noreply.github.com>
2021-07-19 01:32:15 -0400

    Some updates 2

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -195,5 +195,5 @@ STATICFILES_DIRS = [
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 MEDIA_URL = "/media/"
-
+COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 django_heroku.settings(locals())
